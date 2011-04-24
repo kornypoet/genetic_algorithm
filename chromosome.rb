9df95f234ef46b7b083eb7a3533e453e9abce6ee
Travis Dempsey <travis@infochimps.com>
2011-04-24 17:06:56 -0500

    added a spec test the Chromosome class

--- chromosome.rb
+++ chromosome.rb
@@ -1,20 +1,18 @@
 class Chromosome
 
-  attr_accessor :bit_value, :string_value, :fitness
+  attr_reader :bit_value, :string_value, :numeric_value, :fitness
 
-  Nan = 0.0 / 0.0
-
-   @@bit_decoder = {
-    '0000' => 0.0,
-    '0001' => 1.0,
-    '0010' => 2.0,
-    '0011' => 3.0,
-    '0100' => 4.0,
-    '0101' => 5.0,
-    '0110' => 6.0,
-    '0111' => 7.0,
-    '1000' => 8.0,
-    '1001' => 9.0,
+   @@bit_encoding = {
+    '0000' => '0',
+    '0001' => '1',
+    '0010' => '2',
+    '0011' => '3',
+    '0100' => '4',
+    '0101' => '5',
+    '0110' => '6',
+    '0111' => '7',
+    '1000' => '8',
+    '1001' => '9',
     '1010' => '+',
     '1011' => '-',
     '1100' => '*',
@@ -22,41 +20,47 @@ class Chromosome
   }
 
   def initialize bits
+    raise "Bit string must be binary encoded" if bits.match(/[^01]/)
     @bit_value = bits
-    @string_value = parse @bit_value
+    @string_value = correct(decode @bit_value)
+    @numeric_value = compute @string_value
     @fitness = 0.0
   end
 
-  def to_s
-    "Bit Value: #{@bit_value}\nString Value:#{@string_value}\nFitness: #{@fitness}"
+  def decode bits
+    decoded = bits.scan(/..../).map { |gene| @@bit_encoding[gene] }.compact
   end
 
-  def parse chromosome
-    bit_string = ''
+  def correct decoded
+    corrected   = ""
     looking_for = 'digit'
-    chromosome.scan(/..../).each do |bit|
+    decoded.each do |gene|
       case looking_for
       when 'digit'
-        next unless @@bit_decoder[bit].respond_to? :ceil
-        bit_string += @@bit_decoder[bit].to_s
+        next unless gene.match(/[0-9]/)
+        corrected += gene.to_f.to_s
         looking_for = 'symbol'
       when 'symbol'
-        next unless @@bit_decoder[bit].respond_to? :scan
-        bit_string += @@bit_decoder[bit]
+        next unless gene.match(/[\+\-\*\/]/)
+        corrected += gene
         looking_for = 'digit'
       end
     end
-    bit_string.gsub(/\D\z/, '')
+    corrected.gsub(/\D\z/, '')
   end
 
-  def update_fitness! target
-    begin
-      value = eval @string_value
-      @fitness = (1.0 / (target - value)).abs
-      raise ZeroDivisionError if @fitness == Nan
-    rescue ZeroDivisionError
-      @fitness = 0.0
-    end
+  def compute numeric_string
+    value = eval numeric_string
+    ("%.4f" % value).to_f
+  end
+
+  def fitness= number
+    raise "Can only assign fitness a Float" unless number.instance_of? Float
+    @fitness = number
+  end
+
+  def to_s
+    "Bit Value: #{@bit_value}\nString Value: #{@string_value}\nNumeric Value: #{@numeric_value}\nFitness: #{@fitness}"
   end
 
 end
---
+++ chromosome_spec.rb
@@ -0,0 +1,80 @@
+require 'chromosome'
+
+describe Chromosome do
+  before :each do
+    @test_chrome = Chromosome.new test_bits
+  end
+
+  it "should be initiated ONLY with a string of binary-encoded bits" do
+    begin
+      @bad_chrome_two = Chromosome.new 11001100
+    rescue
+    end
+    @bad_chrome.should be nil
+    begin
+      @bad_chrome_one = Chromosome.new '1100aabb'
+    rescue
+    end
+    @bad_chrome.should be nil
+  end
+
+  it "should be able to display its bit value" do
+    @test_chrome.bit_value.should == test_bits
+  end
+
+  it "should be able to decode its bit value" do
+    bits = @test_chrome.bit_value
+    @test_chrome.decode(bits).should == ['+','6','*','8','+','2','-']
+  end
+
+  it "should be able to correct decoded bits into an evaluatable string" do
+    decoded_bits = @test_chrome.decode test_bits
+    @test_chrome.correct(decoded_bits).should == "6.0*8.0+2.0"
+  end
+
+  it "should be able to display its string value" do
+    @test_chrome.string_value.should == "6.0*8.0+2.0"
+  end
+
+  it "should be able to correctly compute a numeric string" do
+    test_val_one   = "6.0*8.0+2.0"
+    test_val_two   = "4.0+6.0/9.0"
+    test_val_three = "5.0-0.0/0.0"
+    test_val_four  = "5.0/0.0+2.0"
+    @test_chrome.compute(test_val_one).should   == 50.0
+    @test_chrome.compute(test_val_two).should   == 4.6667
+    @test_chrome.compute(test_val_three).should == 0.0
+    @test_chrome.compute(test_val_four).should  == 0.0
+  end
+
+  it "should be able to display its numeric value" do
+    @test_chrome.numeric_value.should == 50.0
+  end
+
+  it "should begin with a fitness score of 0.0" do
+    @test_chrome.fitness.should == 0.0
+  end
+
+  it "should be able to update its fitness ONLY if given a Float" do
+    test_val_one = 5
+    test_val_two = 5.0
+    begin
+      @test_chrome.fitness = test_val_one
+    rescue
+    end
+    @test_chrome.fitness.should == 0.0
+    begin
+      @test_chrome.fitness = test_val_two
+    rescue
+    end
+    @test_chrome.fitness.should == test_val_two
+  end
+
+end
+
+# test_bits split into genes and decoded:
+# 1010 0110 1100 1000 1111 1010 0010 1011 101
+# +    6    *    8    nil  +    2    -    drop
+def test_bits
+  '10100110110010001111101000101011101'
+end
